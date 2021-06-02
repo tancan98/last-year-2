7794b407410c24a492d75ae7fb17da2d729b8b96
tancan98 <78886258+tancan98@users.noreply.github.com>
2021-06-02 14:45:33 +1200

    Add files via upload

---
+++ V3/ask user questions.py
@@ -0,0 +1,8 @@
+
+
+
+#set of questions that are asked
+#question 1
+
+    print("\nQuestion: 1|score:{}".format(score))
+    ans=input("What is the smallest country in the world?\na.Monaco\nb.Vatican City\nc.Tuvalu \nYour answer : ").lower()
---
+++ V3/end of quiz summary.py
@@ -0,0 +1,5 @@
+#tell user final score and thank them for playing
+
+print("\nCongratulations {}!".format(name))
+print("Your Final Score Is",score)
+print("Thanks For Playing : ")
